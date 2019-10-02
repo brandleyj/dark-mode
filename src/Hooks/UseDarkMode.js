51b0b97d231b7e3cf56622824055ae533198614e
Justin Brandley <brandleyj@gmail.com>
2019-10-02 18:04:49 -0500

    MVP

--- src/Hooks/UseDarkMode.js
+++ src/Hooks/UseDarkMode.js
@@ -1,5 +1,17 @@
-import { useLocalStorage } from "./UseLocalStorage";
+import { useLocalStorage } from "./useLocalStorage";
+import { useEffect } from "react";
 
-export default function useDarkMode() {
-	const [dark, setDark] = useLocalStorage("");
-}
+const useDarkMode = (key, initialValue) => {
+	const [dark, setDark] = useLocalStorage(key, initialValue);
+
+	useEffect(() => {
+		if (dark === true) {
+			document.querySelector("body").classList.add("dark-mode");
+		} else {
+			document.querySelector("body").classList.remove("dark-mode");
+		}
+	}, [dark]);
+	return [dark, setDark];
+};
+
+export default useDarkMode;
